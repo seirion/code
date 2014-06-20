7806cf06c79f2ba74482a0f03e013785c8e66210
hyungyu.jang <hyungyu.jang@lge.com>
2014-06-20 13:24:02 +0900

    2014 June lge code jam 2nd : template

---
+++ lge/2014/june/1/main.cpp
@@ -0,0 +1,27 @@
+// http://lgecodejam.com
+// lge code jam June, 2014
+// problem.1
+// hyungyu.jang@lge.com
+
+#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+int n;
+int k;
+
+void getInput() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> in[i];
+}
+
+void solve() {
+}
+
+int main() {
+    int num;
+    cin >> num;
+    for (int i = 0; i < num; i++) {getInput(); solve();}
+    return 0;
+}
