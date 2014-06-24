48f989d1cd6929dfa1731ad686a1f48c5fe81d48
hyungyu.jang <seirion@mensakorea.org>
2014-06-24 23:19:01 +0900

    advanced implementation

--- lge/2014/june/3/main.cpp
+++ lge/2014/june/3/main.cpp
@@ -6,22 +6,17 @@
 
 #if defined _EXTERNAL_DEBUGGER && defined _DEBUG
     #include <trace/trace.h>
+    #include <intrin.h>
 #else
     #define trace printf
-#endif
-#ifdef WIN32
-    #include <intrin.h>
-    #define popcount(x) __popcnt(x)
-#else
-    #define popcount(x) __builtin_popcount(x)
 #endif
 
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <set>
-#include <vector>
 #include <functional>
+#include <algorithm>
 
 using namespace std;
 
@@ -35,110 +30,117 @@ class Point { public:
     bool operator <(const Point& p) const {
         return x == p.x ? y < p.y : x < p.x;
     }
-    bool operator >(const Point& p) const { // trick
-        return y == p.y ? x < p.x : y < p.y;
+    bool operator >(const Point& p) const {
+        return x == p.x ? y > p.y : x > p.x;
     }
     int x, y;
 };
 
-int n;
-int in[100 * 1000 + 1][2];
+typedef multiset<Point> MinSet;
+typedef multiset<Point, greater<Point> > MaxSet;
 
-uint32 get_next(uint32 v) {
-    uint32 w;
-    uint32 t = v | (v - 1);
-#ifdef WIN32
-    unsigned long res;
-    _BitScanForward(&res, v);
-    w = (t + 1) | (((~t & -~t) - 1) >> (res + 1));
-#else
-    w = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(v) + 1));
-#endif
-    return w;
+int n;
+MinSet min_xy, min_yx;
+MaxSet max_xy, max_yx;
+
+template<typename T>
+void remove(T &s, const Point p) {
+    typename T::iterator i = s.begin();
+    for (; i != s.end(); i++) {
+        if (i->x == p.x && i->y == p.y) {
+            s.erase(i);
+            return;
+        }
+    }
 }
 
-int simulation(multiset<Point> &for_x, multiset<Point, greater<Point> > &for_y,
-        int left, int right, int top, int bottom) {
+int simulation(int left, int right, int top, int bottom) {
+    MinSet min_xy_(min_xy);
+    MinSet min_yx_(min_yx);
+    MaxSet max_xy_(max_xy);
+    MaxSet max_yx_(max_yx);
     int i;
-    vector<Point> back;
-    multiset<Point>::iterator xi;
-    multiset<Point>::iterator yi;
-    multiset<Point>::reverse_iterator xri;
-    multiset<Point>::reverse_iterator yri;
     for (i = 0; i < left; i++) {
-        xi = for_x.begin();
-        int x = xi->x;
-        int y = xi->y;
-        for_x.erase(xi);
-        for_y.erase(Point(x, y));
-        back.push_back(Point(x,y));
+        Point p = *min_xy_.begin();
+        Point q(p.y, p.x);
+        remove<MinSet>(min_yx_, q);
+        remove<MaxSet>(max_yx_, q);
+        min_xy_.erase(min_xy_.begin());
     }
     for (i = 0; i < right; i++) {
-        xri = for_x.rbegin();
-        int x = xri->x;
-        int y = xri->y;
-        for_x.erase(--xri.base());
-        for_y.erase(Point(x, y));
-        back.push_back(Point(x,y));
-    }
-    for (i = 0; i < top; i++) {
-        yri = for_y.rbegin();
-        int x = yri->x;
-        int y = yri->y;
-        for_y.erase(--yri.base());
-        for_x.erase(Point(x, y));
-        back.push_back(Point(x,y));
+        Point p = *max_xy_.begin();
+        Point q(p.y, p.x);
+        remove<MinSet>(min_yx_, q);
+        remove<MaxSet>(max_yx_, q);
+        max_xy_.erase(max_xy_.begin());
     }
     for (i = 0; i < bottom; i++) {
-        yi = for_y.begin();
-        int x = yi->x;
-        int y = yi->y;
-        for_y.erase(yi);
-        for_x.erase(Point(x, y));
-        back.push_back(Point(x,y));
+        Point p = *min_yx_.begin();
+        Point q(p.y, p.x);
+        remove<MinSet>(min_xy_, q);
+        remove<MaxSet>(max_xy_, q);
+        min_yx_.erase(min_yx_.begin());
     }
-    // calculate
-    xi = for_x.begin();
-    xri = for_x.rbegin();
-    yi = for_y.begin();
-    yri = for_y.rbegin();
-
-    int result = max(xri->x - xi->x, yri->y - yi->y);
-
-    // recover
-    for (i = 0; i < back.size(); i++) {
-        for_x.insert(back[i]);
-        for_y.insert(back[i]);
+    for (i = 0; i < top; i++) {
+        Point p = *max_yx_.begin();
+        Point q(p.y, p.x);
+        remove<MinSet>(min_xy_, q);
+        remove<MaxSet>(max_xy_, q);
+        max_yx_.erase(max_yx_.begin());
     }
 
-    return result;
+    return max(max_xy_.begin()->x - min_xy_.begin()->x, max_yx_.begin()->x - min_yx_.begin()->x);
 }
 
 void solve() {
+    min_xy.clear();
+    min_yx.clear();
+    max_xy.clear();
+    max_yx.clear();
     scanf("%d", &n);
-    int i;
-    multiset<Point> for_x;
-    multiset<Point, greater<Point> > for_y;
+    int i, x, y;
+    for (i = 0; i < 4; i++) {
+        scanf("%d %d", &x, &y);
+        min_xy.insert(Point(x, y));
+        max_xy.insert(Point(x, y));
+        min_yx.insert(Point(y, x));
+        max_yx.insert(Point(y, x));
+    }
 
-    for (i = 0; i < n; i++) {
-        scanf("%d %d", &in[i][X], &in[i][Y]);
-        for_x.insert(Point(in[i][X], in[i][Y]));
-        for_y.insert(Point(in[i][X], in[i][Y]));
+    for (; i < n; i++) {
+        scanf("%d %d", &x, &y);
+        min_xy.insert(Point(x, y));
+        max_xy.insert(Point(x, y));
+        min_yx.insert(Point(y, x));
+        max_yx.insert(Point(y, x));
+        min_xy.erase(--min_xy.rbegin().base());
+        min_yx.erase(--min_yx.rbegin().base());
+        max_xy.erase(--max_xy.rbegin().base());
+        max_yx.erase(--max_yx.rbegin().base());
     }
 
     // for all cases
-    uint32 flag = 0x7; // 111 (2)
     int solution = 0x7FFFFFFF;
-    while (flag <= 0xe00) { // 1110 0000 0000 (2)
-        // 000      000     000     000   (2)
-        // left     right   top     bottom
-        int left = popcount(flag & 0xe00);      // 1110 0000 0000
-        int right = popcount(flag & 0x1c0);     // 0001 1100 0000
-        int top = popcount(flag & 0x38);        // 0000 0011 1000
-        int bottom = popcount(flag & 0x7);      // 0000 0000 0111
-
-        solution = min(solution, simulation(for_x, for_y, left, right, top, bottom));
-        flag = get_next(flag);
+    int direct[4]; // left, right, top, bottom
+    for (int a = 1; a <= 8; a <<= 1) {
+        for (int b = 1; b <= 8; b <<= 1) {
+            for (int c = 1; c <= 8; c <<= 1) {
+                memset(direct, 0, sizeof(int) * 4);
+#ifdef WIN32
+                unsigned long res;
+                _BitScanForward(&res, a); direct[res]++;
+                _BitScanForward(&res, b); direct[res]++;
+                _BitScanForward(&res, c); direct[res]++;
+#else // __linux__
+                direct[__builtin_ctz(a)]++;
+                direct[__builtin_ctz(b)]++;
+                direct[__builtin_ctz(c)]++;
+#endif
+                //static int k = 1;
+                //trace("%d) %d %d %d %d (%d)\n", k++, direct[0], direct[1], direct[2], direct[3], direct[0] + direct[1] + direct[2] + direct[3]);
+                solution = min(solution, simulation(direct[0], direct[1], direct[2], direct[3]));
+            }
+        }
     }
     trace("%d\n", solution);
 }
