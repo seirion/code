7063bbc83926c05c3bc26ea5ea5db9c69bd5af2f
hyungyu.jang <seirion@mensakorea.org>
2014-08-22 21:47:15 +0900

    algospot ordering completed

--- algospot/ordering/main.cpp
+++ algospot/ordering/main.cpp
@@ -3,100 +3,61 @@
 #include <cstdio>
 #include <cstring>
 #include <set>
-#include <map>
-#include <vector>
 #include <list>
+#include <vector>
 
 using namespace std;
 
 int n, depnum;
-char dep[101][3];
-vector<char> unchecked;
-map<char, set<char> >dict;
+bool checked[26];
+set<int> gtable[26];
 
 
 void getInput() {
-
-    cin >> n >> depnum;
-    cin.get();
-
-    bool check [26] = {false,};
+    scanf("%d %d\n", &n, &depnum);
     int i;
+    char a, b;
     for (i = 0; i < depnum; i++) {
-        gets(dep[i]);
-        check[dep[i][0]-'A'] = check[dep[i][1]-'A'] = true;
-    }
-    unchecked.clear();
-    for (i = n-1; i >= 0; i--) { // reverse order
-        if (check[i]) dict[i + 'A'].clear();
-        else unchecked.push_back(i + 'A');
-    }
-    for (i = 0; i < depnum; i++) {
-        dict[dep[i][0]].insert(dep[i][1]);
+        scanf("%c%c\n", &a, &b);
+        gtable[b-'A'].insert(a-'A');
     }
 }
 
-void print(list<char> &solution) {
-    list<char>::reverse_iterator it = solution.rbegin();
-    for (; it != solution.rend(); it++) {
-        printf("%c", *it);
-    }
-    printf("\n");
-}
-
-void remove_checked(set<char> &out, list<char> &solution) {
-    set<char>::reverse_iterator rt;
-    for (rt = out.rbegin(); rt != out.rend(); rt++) {
-        dict.erase(*rt);
-        solution.push_back(*rt);
-    }
-
-    set<char>::const_iterator it;
-    for (it = out.begin(); it != out.end(); it++) {
-        map<char, set<char> >::iterator m = dict.begin();
-        for (; m != dict.end(); m++) {
-            m->second.erase(*it);
-        }
-    }
-}
-
-void push_unchecked(list<char> &solution) {
-    list<char>::iterator i = solution.begin();
-    vector<char>::iterator j = unchecked.begin();
-    while (i != solution.end() && j != unchecked.end()) {
-        if (*i < *j) {
-            solution.insert(i, *j);
-            j++;
-        }
-        else {
-            i++;
-        }
-    }
-
-    while (i == solution.end() && j != unchecked.end()) {
-        solution.push_back(*j);
-        j++;
+void remove(int x) {
+    for (int i = 0; i < n; i++) {
+        gtable[i].erase(x);
     }
 }
 
 void solve() {
-    list<char> solution;
-
-    while (!dict.empty()) {
-        set<char> out;
-        map<char, set<char> >::iterator i = dict.begin();
-        for (; i != dict.end(); i++) {
-            char key = i->first;
-            set<char> &value = i->second;
-            if (value.empty()) {
-                out.insert(key);
-            }
-        }
-        remove_checked(out, solution);
+    memset(checked, 0, sizeof(bool) * 26);
+    int i;
+    list<int> r;
+    vector<char> solution;
+    for (i = 0; i < n; i++) { //init
+        r.push_back(i);
     }
 
-    push_unchecked(solution);
-    print(solution);
+    while (!r.empty()) {
+        list<int>::iterator it = r.begin();
+        for (; it != r.end(); it++) {
+            if (gtable[*it].empty()) {
+                //solution.push_back((char)(*it + 'A'));
+                cout << (char)(*it + 'A');
+                remove(*it);
+                r.erase(it);
+                break;
+            }
+        }
+    }
+
+    cout << endl;
+
+    for (i = 0; i < n; i++) {
+        gtable[i].clear();
+    }
+
+    
 }
 
 int main(int, char *[]) {
