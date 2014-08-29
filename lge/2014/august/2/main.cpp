e0d2dc567126a69eb52797a19b74156b489550dc
hyungyu.jang <seirion@mensakorea.org>
2014-08-29 19:45:58 +0900

    lgecodejam 2014 august problem 2 completed

--- lge/2014/august/2/main.cpp
+++ lge/2014/august/2/main.cpp
@@ -20,7 +20,7 @@ int in2 [100 * 1000 + 1];
 
 // return (in1 < in2);
 int less(int index, int start, int target, int mid) {
-    if (in2[index] == mid) return 0;
+    if (in1[index] == mid || in2[index] == mid) return 0;
     if (in1[index] == start && in2[index] == target) return 1;
     if (in1[index] == start) return less(index-1, start, mid, target);
     if (in1[index] == target) return less(index-1, mid, target, start);
@@ -28,6 +28,24 @@ int less(int index, int start, int target, int mid) {
     // something wrong
     printf("=================\n");
     return 0;
+}
+
+bool valid(int in[], int index, int start, int target, int mid) {
+
+    if (in[index] == mid) return false;
+    if (index == 1) return true;
+    if (in[index] == start) {
+        return valid(in, index-1, start, mid, target);
+    }
+    else {
+        return valid(in, index-1, mid, target, start);
+    }
+}
+
+int valid(int n) {
+    if (!valid(in1, n, 1, 2, 3)) return 0;
+    if (!valid(in2, n, 1, 2, 3)) return 0;
+    return less(n, 1, 2, 3);
 }
 
 void solve() {
@@ -42,7 +60,7 @@ void solve() {
         in2[i] = a;
     }
 
-    printf("%d\n", less(n, 1, 2, 3));
+    printf("%d\n", valid(n));
 }
 
 int main(int, char*[]) {
