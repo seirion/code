967b1e656067df1163246d11a38c1edbffcb6921
hyungyu.jang <hyungyu.jang@lge.com>
2014-09-17 20:43:01 +0900

    template files

---
+++ algospot/template/main.cpp
@@ -0,0 +1,20 @@
+// http://algospot.com/judge/problem/read/
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void input() {
+    scanf("%d", &n);
+}
+
+void solve() {
+}
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++) {input(); solve();}
+    return 0;
+}
