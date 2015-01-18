1293470d966b69a3c07c106bfa968071c0eef154
seirion <seirion@mensakorea.org>
2015-01-18 18:19:37 +0900

    Facebook Hacker Cup 2015 Round 1
    4. Corporate Gifting

---
+++ facebook/2015/round1/4/main.cpp
@@ -0,0 +1,104 @@
+// https://www.facebook.com/hackercup/problems.php?pid=759650454070547&round=344496159068801
+// Facebook Hacker Cup 2015 Round 1
+// 4. Corporate Gifting
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class Tree { public:
+    Tree() : id(0), pid(0) {}
+    //Tree(int i, int p) : id(i), pid(p) {}
+    int id, pid;
+    vector<int> child;
+    void clear() { child.clear(); }
+};
+
+Tree tree[200*1000 + 1];
+int memo[200*1000 + 1][3]; // 0 1 2
+
+void clear(int n) {
+    for (int i = 1; i <= n; i++) {
+        tree[i].clear();
+    }
+}
+
+int dfs(int id, int value) { // depth-first-search
+    int &cost = memo[id][value];
+    if (cost != 0) return cost;
+    cost = (value == 0 ? 3 : value);
+
+    // child shoud be {1,2,3} - {value}
+    vector<int> &child = tree[id].child;
+    int size = child.size();
+    for (int i = 0; i < size; i++) {
+        cost += min(
+                dfs(child[i], (value+1) % 3),
+                dfs(child[i], (value+2) % 3)
+                );
+    }
+    return cost;
+}
+
+void make_table(int n) {
+    for (int i = n; i >= 1; i--) {
+        if (tree[i].child.empty()) {
+            memo[i][0] = 3;
+            memo[i][1] = 1;
+            memo[i][2] = 2;
+        }
+        else {
+            for (int x = 0; x < 3; x++) {
+                memo[i][x] = (x == 0 ? 3 : x);
+                
+                vector<int> &child = tree[i].child;
+                int size = child.size();
+                for (int j = 0; j < size; j++) {
+                    int idx = child[j];
+                    memo[i][x] += min(
+                            memo[idx][(x+1) % 3],
+                            memo[idx][(x+2) % 3]
+                            );
+                }
+            }
+        }
+    }
+}
+
+void solve() {
+    memset(memo, 0, sizeof(int) * (200 * 1000 + 1) * 3);
+
+    int n;
+    scanf("%d\n", &n);
+    for (int i = 1; i <= n; i++) {
+        int pid; // pid
+        scanf("%d\n", &pid);
+        Tree &now = tree[i];
+        now.id = i;
+        now.pid = pid;
+        tree[pid].child.push_back(i);
+    }
+
+    make_table(n);
+    /////////////////////////////////////////////////////////////////////////
+    int mini = 0x7FFFFFFF;
+    for (int i = 0; i < 3; i++) {
+        //mini = min(mini, dfs(1, i));
+        mini = min(mini, memo[1][i]);
+    }
+    printf("%d\n", mini);
+    clear(n);
+}
+
+int main() {
+    int c;
+    scanf("%d", &c);
+    for (int i = 0; i < c; i++) {
+        printf("Case #%d: ", i + 1);
+        solve();
+    }
+    return 0;
+}
+
