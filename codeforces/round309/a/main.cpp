4d82db53289b1340de42106591699b2e318288bb
hyungyu.jang <hyungyu.jang@lge.com>
2015-06-25 14:26:58 +0900

    codeforces round309 A
    A. Kyoya and Photobooks

---
+++ codeforces/round309/a/main.cpp
@@ -0,0 +1,12 @@
+// http://codeforces.com/contest/554/problem/A
+// A. Kyoya and Photobooks
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    cout << (s.size() + 1) * 25 + 1 << endl;
+    return 0;
+}
