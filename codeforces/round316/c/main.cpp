9d6c168dd916fe1fd658b36ba438649e6a4be047
seirion <seirion@mensakorea.org>
2015-08-19 19:24:14 +0900

    codeforces round316 C
    C - Replacement

---
+++ codeforces/round316/c/main.cpp
@@ -0,0 +1,59 @@
+// codeforces round316
+// http://codeforces.com/contest/570/problem/C
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int n, query;
+char in[300003];
+vector<int> index;
+vector<int> num;
+int total(0);
+
+void set_table() {
+    bool dot(false);
+    int size = n+2;
+
+    for (int i = 0; i < size; i++) {
+        if (dot && in[i] != '.') {
+            dot = false;
+            num.push_back(i-index.back());
+            total += num.back() - 1;
+        }
+        else if (!dot && in[i] == '.') {
+            dot = true;
+            index.push_back(i);
+        }
+    }
+}
+
+int check(int i, char c) {
+    int plus(0);
+    if (c == '.' && in[i] != '.') {
+        if (in[i-1] == '.') plus++;
+        if (in[i+1] == '.') plus++;
+    }
+    else if (c != '.' && in[i] == '.') {
+        if (in[i-1] == '.') plus--;
+        if (in[i+1] == '.') plus--;
+    }
+    return total + plus;
+}
+
+int main() {
+    scanf("%d %d\n", &n, &query);
+    fgets(in+1, sizeof(in)-1, stdin); 
+    in[0] = 'x';
+
+    set_table();
+    while (query--) {
+        int i; char c;
+        scanf("%d %c\n", &i, &c);
+        total = check(i, c);
+        in[i] = c;
+        cout << total << "\n";
+    }
+    return 0;
+}
