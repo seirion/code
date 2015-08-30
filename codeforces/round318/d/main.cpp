217abeb4c5bb6d227eac7206949291453f90aff5
seirion <seirion@mensakorea.org>
2015-08-30 13:51:04 +0900

    codeforces round318
    D - Bear and Blocks : easy way

--- codeforces/round318/d/main.cpp
+++ codeforces/round318/d/main.cpp
@@ -1,29 +1,26 @@
 // codeforces round318
 // http://codeforces.com/contest/574/problem/D
 #include <iostream>
-#include <deque>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int n;
-deque<int> in;
+vector<int> in;
 
 void solve() {
-    int c(0), even(0);
     in[0] = 1;
-
     for (int i = 1; i < n; i++) {
         if (in[i-1] < in[i]) {
             in[i] = in[i-1] + 1;
         }
-        else {
-            int x = i;
-            while (0 < x) {
-                if (in[x-1] <= in[x]) break;
-                in[x-1] = in[x] + 1;
-                x--;
-            }
+    }
+
+    in[n-1] = 1;
+    for (int i = n-2; i >= 0; i--) {
+        if (in[i+1] < in[i]) {
+            in[i] = in[i+1] + 1;
         }
     }
 
@@ -36,8 +33,6 @@ int main() {
         int x; cin >> x;
         in.push_back(x);
     }
-    in.push_back(0);
-    n++;
     solve();
 
     return 0;
