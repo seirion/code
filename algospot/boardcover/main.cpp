059319dc8076a5acb46fb7d8806f5b2c98821891
hyungyu.jang <hyungyu.jang@lge.com>
2015-09-04 14:36:41 +0900

    rewrite algospot boardcover

--- algospot/boardcover/main.cpp
+++ algospot/boardcover/main.cpp
@@ -6,33 +6,31 @@
 using namespace std;
 
 class Tile { public:
-    Tile() {}
-    Tile(int x, int y) : h(x), w(y) {}
-    int h, w;
+    Tile(int r_ = 0, int c_ = 0) : r(r_), c(c_) {}
+    int r, c;
     bool operator < (const Tile &t) const {
-        return (h == t.h ? w < t.w : h < t.h);
+        return (r == t.r ? c < t.c : r < t.r);
     }
 };
 
-const int XX [4][3][2] = {
+const int CAND [4][3][2] = {
     {{0, 0}, {0, 1} ,{1, 1}},
     {{0, 0}, {1, 0} ,{0, 1}},
     {{0, 0}, {1, 0} ,{1, 1}},
     {{0, 0}, {1, 0} ,{1, -1}}
 };
 
-int h, w, total, count;
-char in[20][21];
+int h, w;
+char in[20][22];
 set<Tile> remain;
 
-
-void getInput() {
+void input() {
     cin >> h >> w;
     cin.get();
 
     remain.clear();
     for (int i = 0; i < h; i++) {
-        gets(in[i]);
+        fgets(in[i], 22, stdin);
         for (int j = 0; j < w; j++) {
             if (in[i][j] == '.') remain.insert(Tile(i, j));
         }
@@ -41,60 +39,53 @@ void getInput() {
 
 bool canbe(int index, int row, int col) {
     for (int i = 0; i < 3; i++) {
-        int r = row + XX[index][i][0];
-        int c = col + XX[index][i][1];
+        int r = row + CAND[index][i][0];
+        int c = col + CAND[index][i][1];
         if (remain.find(Tile(r, c)) == remain.end()) return false;
     }
     return true;
 }
 
-void be(int index, int row, int col) {
+void cover(int index, int row, int col) {
     for (int i = 0; i < 3; i++) {
-        int r = row + XX[index][i][0];
-        int c = col + XX[index][i][1];
+        int r = row + CAND[index][i][0];
+        int c = col + CAND[index][i][1];
         remain.erase(Tile(r, c));
     }
 }
 
 void recover(int index, int row, int col) {
     for (int i = 0; i < 3; i++) {
-        int r = row + XX[index][i][0];
-        int c = col + XX[index][i][1];
+        int r = row + CAND[index][i][0];
+        int c = col + CAND[index][i][1];
         remain.insert(Tile(r, c));
     }
 }
 
-void cover() {
+int get_possible() {
     if (remain.size() == 0) {
-        count++;
-        return;
+        return 1;
     }
 
+    int count = 0;
     Tile c = *remain.begin();
     for (int i = 0; i < 4; i++) {
-        if (canbe(i, c.h, c.w)) {
-            be(i, c.h, c.w);
-            cover();
-            recover(i, c.h, c.w);
+        if (canbe(i, c.r, c.c)) {
+            cover(i, c.r, c.c);
+            count += get_possible();
+            recover(i, c.r, c.c);
         }
     }
+    return count;
 }
 
 void solve() {
-    count = 0;
-    total = remain.size();
-    if (total % 3 != 0) {
-        cout << 0 << endl;
-        return;
-    }
-
-    cover();
-    cout << count << endl;
+    cout << (remain.size() % 3 == 0 ? get_possible() : 0) << endl;
 }
 
 int main(int argc, char *argv[]) {
     int num;
     cin >> num;
-    for (int i = 0; i < num; i++) {getInput(); solve();}
+    for (int i = 0; i < num; i++) {input(); solve();}
     return 0;
 }
