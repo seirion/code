703ad529c10879db33e647c57d3ae6315adf2e31
hyungyu.jang <hyungyu.jang@lge.com>
2015-09-17 10:52:46 +0900

    improve performance : use vector instead of set
    iterating of vector is faster than that of set

--- lge/2015/coding_expert/3/main.cpp
+++ lge/2015/coding_expert/3/main.cpp
@@ -1,22 +1,22 @@
 // LGE codejam 2015
 // 3. ... ...
 #include <iostream>
-#include <set>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int n, m;
-set<int> in[20001];
+vector<int> in[20001];
 
 void input() {
     cin >> n >> m;
     for (int i = 0; i < m; i++) {
         int a, b; cin >> a >> b;
-        in[a].insert(b);
-        in[b].insert(a);
+        in[a].push_back(b);
+        in[b].push_back(a);
     }
+    for (int i = 1; i <= n; i++) sort(in[i].begin(), in[i].end());
 }
 
 int common_friends(int a, int b) { // a != b
@@ -30,7 +30,9 @@ void solve() {
     for (int a = 1; a <= n; a++) {
         if (in[a].size() <= best) continue;
         for (int b = a+1; b <= n; b++) {
-            if (in[a].find(b) != in[a].end() || in[b].size() <= best) continue;
+            if (in[b].size() <= best) continue;
+            auto it = lower_bound(in[a].begin(), in[a].end(), b);
+            if (it != in[a].end() && *it == b) continue;
             best = max(best, common_friends(a, b));
         }
     }
