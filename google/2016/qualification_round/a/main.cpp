5023b0bbcfe922958aacb0a418d0125ece0f099c
seirion <seirion@mensakorea.org>
2016-04-10 18:34:43 +0900

    google codejam 2016 Qualification Round problem D
    Problem D. Fractiles

--- google/2016/qualification_round/a/main.cpp
+++ google/2016/qualification_round/a/main.cpp
@@ -8,51 +8,29 @@
 
 using namespace std;
 
-bool put(set<char> &s, const string &str) {
-    for (char c : str) { s.insert(c); }
+bool put(set<char> &s, int num) {
+    while (num) {
+        s.insert(num % 10);
+        num /= 10;
+    } 
     return s.size() == 10;
 }
 
-void add(string &now, string &in) {
-    int size = in.size();
-    int prev = 0;
-    int i;
-    for (i = 0; i < size; i++) {
-        now[i] += in[i] + prev;
-        prev = now[i] / 10;
-        now[i] = now[i] % 10;
-    }
-
-    size = now.size();
-    for (; i < size && 0 < prev; i++) {
-        now[i] += prev;
-        prev = now[i] / 10;
-        now[i] = now[i] % 10;
-    }
-
-    if (0 < prev) now.push_back(prev);
-}
-
 void solve() {
-    string in; cin >> in;
-    if (in == "0") {
+    int in; cin >> in;
+    if (in == 0) {
         cout << "INSOMNIA\n";
         return;
     }
 
-    reverse(in.begin(), in.end());
-    for (char &c : in) c -= '0';
-
-    string now = in;
+    int now = in;
     set<char> s;
-    int trying = 0;
     while (true) {
         if (put(s, now)) break;
-        add(now, in);
+        now += in;
     }
 
-    for (auto it = now.rbegin(); it != now.rend(); it++) cout << (int)*it;
-    cout << endl;
+    cout << now << endl;
 }
 
 int main() {
