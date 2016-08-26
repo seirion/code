6be9ff38d6da0ac95e70f27ff0b12483a84aa166
neo.jang <neo.jang@hpcnt.com>
2016-08-26 16:54:46 +0900

    simplify

--- lge/2016/2/1/main.cpp
+++ lge/2016/2/1/main.cpp
@@ -4,16 +4,9 @@
 
 using namespace std;
 
-int x, y, n;
-
-double get_theta(int x1, int y1, int x2, int y2) {
-    double len = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-    return acos(y / len);
-}
-
 void solve() {
-    cin >> x >> y >> n;
-    double theta = M_PI*2 - get_theta(0, 0, x, y)*2 - get_theta(n, 0, x, y)*2;
+    int x, y, n; cin >> x >> y >> n;
+    double theta = M_PI*2 - atan((double)x / y)*2 - atan((double)(n-x) / y)*2;
     cout << n + (int)(y*theta) + 1 << endl;
 }
 
