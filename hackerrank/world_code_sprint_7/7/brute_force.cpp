4b1ed79b71817d55a330f63a06d4202ea985785b
seirion <seirion@mensakorea.org>
2016-09-25 20:40:53 +0900

    simplify

--- hackerrank/world_code_sprint_7/7/brute_force.cpp
+++ hackerrank/world_code_sprint_7/7/brute_force.cpp
@@ -10,22 +10,21 @@ using namespace std;
 int n, q;
 string in;
 
-map<int, vector<int>> mm(int a, int b) {
-    map<int, vector<int>> m;
+vector<int> kk(int a, int b) {
+    vector<int> vv;
+
     map<char, int> k; // key
     int num = 0;
+
     for (int i = a; i < b; i++) {
         char &c = in[i];
 
-        if (k.find(c) != k.end()) {
-            int key = k[c];
-            m[key].push_back(i-a);
-        }
-        else {
+        if (k.find(c) == k.end()) {
             k[c] = num++;
         }
+        vv.push_back(k[c]);
     }
-    return m;
+    return vv;
 }
 
 int solve(int a, int b) {
@@ -34,10 +33,10 @@ int solve(int a, int b) {
 
     int limit = n - len + 1;
     long long r = 0;
-    map<int, vector<int>> org = mm(a, b);
+    vector<int> org = kk(a, b);
 
     for (int from = 0; from < limit; from++) {
-        if (org == mm(from, from+len)) r++;
+        if (org == kk(from, from+len)) r++;
     }
     return r;
 }
