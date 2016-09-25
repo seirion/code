96edacbac15aa2f4c0c0d058cb02b4a94bb11af4
seirion <seirion@mensakorea.org>
2016-09-25 15:54:23 +0900

    correct typo error

--- short/modular_inverse.cpp
+++ short/modular_inverse.cpp
@@ -7,7 +7,7 @@ const long long MOD = 1000000007; // 10^9 + 7
 long long fast_power(long long base, long long p) {
     if (p == 1) return base;
 
-    long long res = fast_power(base, p<<1);
+    long long res = fast_power(base, p>>1);
     res = (res * res) % MOD;
     if (p & 1) res = res * base;
     return res % MOD;
