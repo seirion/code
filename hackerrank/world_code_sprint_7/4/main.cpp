c638350257ee406797e442415954718aaff8af70
seirion <seirion@mensakorea.org>
2016-09-25 13:07:59 +0900

    World CodeSprint 7 #4

---
+++ hackerrank/world_code_sprint_7/4/main.cpp
@@ -0,0 +1,65 @@
+// https://www.hackerrank.com/contests/world-codesprint-7/challenges/summing-pieces
+// Summing Pieces
+// O(n^2) solution
+#include <iostream>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+int n;
+long long in[1000001] = {0, };
+long long m[1000001] = {0, };
+long long pre[1000001] = {0, };
+
+
+void build() {
+    pre[0] = pre[1] = 1;
+    for (int i = 2; i <= 1000000; i++) pre[i] = pre[i-1] * 2 % MOD;
+}
+
+long long fast_power(long long base, long long p) {
+    if (p == 1) return base;
+
+    long long res = fast_power(base, p>>1);
+    res = (res * res) % MOD;
+    if (p & 1) res = res * base;
+    return res % MOD;
+}
+
+long long inv(int a) { // modular multiplicative inverse
+    return fast_power(a, MOD-2);
+}
+
+long long base(int limit) {
+    long long r = 1;
+    for (int i = 1; i < limit; i++) r = (r * 2 + 1) % MOD;
+    return r;
+}
+
+void make() {
+
+    m[0] = m[n-1] = base(n);
+    int size = (n + 1) / 2;
+    long long x = 1;
+    for (int i = 1; i < size; i++) {
+        long long more = pre[n-i] - pre[i];
+        m[n-1-i] = m[i] = (m[i-1] + more) % MOD;
+    }
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> in[i];
+
+    build();
+    make();
+
+    long long r = 0;
+    for (int i = 0; i < n; i++) {
+        long long temp = m[i] * in[i] % MOD;
+        r = (r + temp) % MOD;
+    }
+
+    cout << r << endl;
+    return 0;
+}
