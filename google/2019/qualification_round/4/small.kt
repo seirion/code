d99ab8f88be1a11077df8e53af12e9ad0116a8a2
babo <seirion@mensakorea.org>
2019-04-08 01:27:22 +0900

    codejame 2019 Q-round #4 (for small input)

---
+++ google/2019/qualification_round/4/small.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.toInt()
+    (1..t).forEach {
+        solve()
+    }
+}
+
+fun solve() {
+    val (N, B, F) = readLine()!!.split("\\s".toRegex()).map { it.toInt() }
+
+    val response = ArrayList<String>()
+    (0 until F).forEach {
+        println(bitFlag(N, 1 shl it))
+        readLine()!!.let { s -> response.add(s) }
+    }
+
+
+    val s = IntArray(N - B) { i -> 0 }
+    response.forEachIndexed { row, value ->
+        value.forEachIndexed { col, v -> if (v == '1') s[col] += (1 shl row) }
+    }
+
+    println((0 until N).toSet().minus(s.toList()).joinToString(" "))
+    readLine()
+}
+
+fun bitFlag(size: Int, gap: Int) =
+        ("0".repeat(gap) + "1".repeat(gap))
+                .repeat(size / gap + gap)
+                .take(size)
+
