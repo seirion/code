f25636324bfcf5acfb3a738e4606bd0eda6188af
babo <seirion@mensakorea.org>
2019-07-08 19:26:55 +0900

    fix wrong answer

--- facebook/2019/round1/1/main.kt
+++ facebook/2019/round1/1/main.kt
@@ -10,81 +10,38 @@ fun main(args: Array<String>) {
 
 data class T(val a: Int, val b: Int, val c: Int)
 
-var parent = IntArray(1001) // membership
-var connect = ArrayList<HashMap<Int, Int>>()
-
-fun clear() {
-    connect.clear()
-    repeat(1001) {
-        parent[it] = 0
-        connect.add(HashMap())
-    }
-}
-
 fun solve() {
-    clear()
     val (n, m) = readLine()!!.split(" ").map { it.toInt() }
     val input = ArrayList<T>()
+    val dist = ArrayList<IntArray>()
+
+    repeat(n + 1) {
+        dist.add(IntArray(n + 1) { 1000000000 } )
+    }
     repeat(m) {
         val (a, b, c) = readLine()!!.split(" ").map { it.toInt() }
-        input.add(T(Math.min(a, b), Math.max(a, b), c))
+        input.add(T(a, b, c))
+        dist[a][b] = c
+        dist[b][a] = c
     }
-    input.sortWith(Comparator { x, y -> x.c.compareTo(y.c) })
 
-    input.forEach {
-        val groupA = group(it.a)
-        val groupB = group(it.b)
-        if (groupA != groupB) {
-            parent[groupA] = groupB // merge
+    (1..n).forEach { i ->
+        (1..n).forEach { j ->
+            (1..n).forEach { k ->
+                dist[j][k] = Math.min(dist[j][k], dist[j][i] + dist[i][k])
+            }
         }
-        connect[it.a][it.b] = it.c
-        connect[it.b][it.a] = it.c
     }
 
     input.forEach {
-        val d = distance(it.a, it.b)
-        if (it.c != d) {
+        if (dist[it.a][it.b] != it.c) {
             println("Impossible")
             return
         }
     }
-
-    val out = ArrayList<T>()
-    connect.forEachIndexed { a, hashMap ->
-        hashMap.forEach { b, c -> out.add(T(a, b, c)) }
+    println(m)
+    input.forEach {
+        println("${it.a} ${it.b} ${it.c}")
     }
-
-    out.sortWith(Comparator { x, y ->
-        if (x.a == y.a) x.b.compareTo(y.b)
-        x.a.compareTo(y.a)
-    })
-
-    println(out.size / 2)
-    out.filterIndexed { i, _ -> i % 2 == 0 }
-            .forEach { println("${it.a} ${it.b} ${it.c}") }
 }
 
-fun group(a: Int): Int {
-    if (parent[a] == 0) return a
-    parent[a] = group(parent[a])
-    return parent[a]
-}
-
-fun distance(a: Int, b: Int): Int {
-    val q = PriorityQueue<Pair<Int, Int>> { a, b -> a.first.compareTo(b.first) }
-    val visit = BooleanArray(1001) { false }
-    q.add(Pair(0, a))
-
-    while (q.isNotEmpty()) {
-        val now = q.poll()
-        if (now.second == b) return now.first
-        visit[now.second] = true
-
-        connect[now.second].forEach { where, dist ->
-            if (!visit[where]) {
-                q.add(Pair(dist + now.first, where))
-            }
-        }
-    }
-    return -1
-}
