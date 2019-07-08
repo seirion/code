4937b282f702e09badc94765166707c25733a391
babo <seirion@mensakorea.org>
2019-07-08 21:38:53 +0900

    fix wrong answers

--- facebook/2019/round1/2/main.kt
+++ facebook/2019/round1/2/main.kt
@@ -1,4 +1,9 @@
+import java.util.*
+
 fun main(args: Array<String>) {
+    powOf2.add(1)
+    repeat(1000000) { powOf2.add(powOf2.last() * 2 % MOD) }
+
     val t = readLine()!!.toInt()
     repeat(t) {
         print("Case #${it + 1}: ")
@@ -7,37 +12,24 @@ fun main(args: Array<String>) {
 }
 
 const val MOD = 1000000007
+val powOf2 = ArrayList<Int>()
 
 fun solve() {
     val(n, k) = readLine()!!.split(" ").map { it.toInt() }
-    val str = readLine()!!.reversed()
-
-    var a = 0
     var b = 0
     var out = 0L
-    str.forEachIndexed { i, c ->
-        if (c == 'A') a++
-        else b++
-
-        if (b - a > k) {
-            out = (out + powOf2(n - i)) % MOD
-            a++
-            b--
-        } else if (a > b) {
-            a = b
-        }
-    }
-
+    readLine()?.reversed()
+            ?.mapIndexed { i, c -> Pair(n - i, c) }
+            ?.forEach {
+                if (it.second == 'B') {
+                    if (b < k) {
+                        b++
+                        return@forEach
+                    }
+                    out = (out + powOf2[it.first]) % MOD
+                }
+                b = Math.max(b - 1, 0)
+            }
     println(out)
 }
 
-fun powOf2(n: Int): Long {
-    if (n == 0) return 1L
-    if (n == 1) return 2L
-    val half = powOf2(n / 2)
-    var out = half * half % MOD
-    if (n % 2 == 1) {
-        out = out * 2 % MOD
-    }
-    return out
-}
