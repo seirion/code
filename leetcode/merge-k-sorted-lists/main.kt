fde88df2e652a17ceea1bec8e5cb1b5d9f10023e
babo <seirion@mensakorea.org>
2019-11-15 19:16:50 +0900

    shorter one

--- leetcode/merge-k-sorted-lists/main.kt
+++ leetcode/merge-k-sorted-lists/main.kt
@@ -11,50 +11,20 @@ import java.util.*
  *     var next: ListNode? = null
  * }
  */
-
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-
-    override fun toString(): String {
-        val out = ArrayList<Int>()
-        var i: ListNode? = this
-        while (i != null) {
-            out.add(i.`val`)
-            i = i.next
-        }
-        return out.joinToString(" -> ")
-    }
-}
-
 class Solution {
-    data class Data(val value: Int, val index: Int) : Comparable<Data> {
-        override fun compareTo(other: Data): Int {
-            return value.compareTo(other.value)
-        }
-    }
-
     fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-        val candidate = ArrayList<ListNode?>()
-        candidate.addAll(lists)
-
-        val q = PriorityQueue<Data>()
-        lists.forEachIndexed { i, v ->
-            if (v != null) {
-                q.add(Data(v.`val`, i))
-                candidate[i] = v.next
-            }
-        }
+        val q = PriorityQueue<ListNode> { a, b -> a.`val` - b.`val` }
+        q.addAll(lists.filterNotNull())
 
         val root = ListNode(0)
-        var out: ListNode? = root
+        var current: ListNode? = root
         while (q.isNotEmpty()) {
             val top = q.poll()
-            out!!.next = ListNode(top.value)
-            out = out.next
+            current?.next = ListNode(top.`val`)
+            current = current?.next
 
-            candidate[top.index]?.let {
-                q.add(Data(it.`val`, top.index))
-                candidate[top.index] = it.next
+            top.next?.let {
+                q.add(it)
             }
         }
         return root.next
