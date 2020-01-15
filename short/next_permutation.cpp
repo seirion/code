c268d87b561ac5d730bb9842055b65ff0c200922
babo <seirion@mensakorea.org>
2020-01-15 20:36:42 +0900

    next permutation : cpp implementation

---
+++ short/next_permutation.cpp
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/next-permutation/solution/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // finding first decreasing element from the last
+        int i;
+        for (i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] < nums[i+1]) break;
+        }
+
+        if (i == -1) { // all decreasing order
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int one = i;
+
+        // finding number lager than nums[one]
+        for (; i < nums.size(); i++) {
+            if (nums[one] > nums[i]) break;
+        }
+        int two = i - 1;
+
+        // swap
+        swap(nums[one], nums[two]);
+
+        // reverse
+        auto it = nums.begin();
+        advance(it, one + 1);
+        reverse(it, nums.end());
+    }
+};
+
+int main() {
+    vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    vector<int> org = v;
+    do {
+        for (auto i : v) cout << i << " "; cout << endl;
+        Solution().nextPermutation(v);
+    } while (v != org);
+}
