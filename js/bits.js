fc2dc22c796e8437fc4632c9d438e9b423b29a04
babo.neo <seirion@mensakorea.org>
2020-09-14 15:09:44 +0900

    Create bits.js

---
+++ js/bits.js
@@ -0,0 +1,14 @@
+
+// count tailing zeros
+// https://github.com/mikolalysenko/count-trailing-zeros
+function ctz(v) {
+	let c = 32;
+	v &= -v;
+	if (v) c--;
+	if (v & 0x0000FFFF) c -= 16;
+	if (v & 0x00FF00FF) c -= 8;
+	if (v & 0x0F0F0F0F) c -= 4;
+	if (v & 0x33333333) c -= 2;
+	if (v & 0x55555555) c -= 1;
+	return c;
+}
