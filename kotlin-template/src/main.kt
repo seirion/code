ae2e313966f08db513508ccb378a6732ecf6db2a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-12 17:21:04 +0800

    Delete kotlin-template directory

--- kotlin-template/src/main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    println(": ${getInts()}")
-}
-
-fun getInt() = readLine()!!.toInt()
-fun getInts() = readLine()!!.split(" ").map { it.toInt() }
-fun getLong() = readLine()!!.toLong()
-fun getLongs() = readLine()!!.split(" ").map { it.toLong() }
